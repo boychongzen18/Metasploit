78b4bcf9a730f9a9594d66d2c1f0402259a95230
boychongzen18 <38745550+boychongzen18@users.noreply.github.com>
2018-05-04 03:26:03 +0700

    Add files via upload

---
+++ metasploit.sh
@@ -0,0 +1,17 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+pkg install python
+
+pkg install python2
+
+pkg install curl
+
+pkg install wget
+
+pkg install perl
+
+pkg install ruby
+
+pkg install php
+
+wget https://Auxilus.github.io/metasploit.sh
